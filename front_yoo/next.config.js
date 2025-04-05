b8390c2c6e65cd35ce0403f8b4ad2301bdd162fd
guzus <storminggalaxys3@gmail.com>
2025-04-06 04:55:58 +0900

    Add output configuration to Next.js config

--- front_yoo/next.config.js
+++ front_yoo/next.config.js
@@ -2,10 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: "export",
   env: {
     NEXT_PUBLIC_HOST_PRIVATE_KEY: process.env.NEXT_PUBLIC_HOST_PRIVATE_KEY,
     NEXT_PUBLIC_CLIENT_PRIVATE_KEY: process.env.NEXT_PUBLIC_CLIENT_PRIVATE_KEY,
   },
-}
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig;
